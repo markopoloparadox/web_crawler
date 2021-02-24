d977b9da39ee2c8a0fa6e8b7f765fa6af77981a0
Marko Petrlic <petrlic.marko@gmail.com>
2021-02-24 13:01:58 +0100

    Added code for normalizing urls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use http_req::response::StatusCode;
-use scraper::{selector, Html, Selector};
+use scraper::{Html, Selector};
+use url::Url;
 
 /*
     What to do: Implement web crawler that finds all links inside a specific domain
@@ -10,21 +11,20 @@ use scraper::{selector, Html, Selector};
 
 fn main() {
     // Fixed domain
-    const target_uri: &str = "https://www.foi.unizg.hr/en";
+    const DOMAIN_ADDRESS: &str = "https://www.foi.unizg.hr/en";
 
-    let html = fetch_html_document(target_uri).unwrap();
-    scrap_links(&html);
+    let html = fetch_html_document(DOMAIN_ADDRESS).unwrap();
+    let mut links = scrap_links(DOMAIN_ADDRESS, &html).unwrap();
+
+    // Remove duplicates
+    links.sort();
+    links.dedup();
 }
 
 pub fn fetch_html_document(url: &str) -> Option<Html> {
     let mut response_body = Vec::new();
-    let response = http_req::request::get(url, &mut response_body);
+    let response = http_req::request::get(url, &mut response_body).ok()?;
 
-    if response.is_err() {
-        println!("Unable to get the html document from url: {}", url);
-    }
-
-    let response = response.unwrap();
     if response.status_code() != StatusCode::new(200) {
         let code = response.status_code();
         let reason = response.reason();
@@ -35,22 +35,34 @@ pub fn fetch_html_document(url: &str) -> Option<Html> {
         return None;
     }
 
-    let document = String::from_utf8(response_body);
-    if document.is_err() {
-        println!("Unable to parse html document from url: {}", url);
-        return None;
-    }
-    let document = document.unwrap();
-
+    let document = String::from_utf8(response_body).ok()?;
     Some(Html::parse_document(document.as_str()))
 }
 
-pub fn scrap_links(html: &Html) -> Option<Vec<String>> {
+pub fn scrap_links(domain_address: &str, html: &Html) -> Option<Vec<String>> {
     let selector = Selector::parse("a[href]").ok()?;
+
+    let mut links = Vec::new();
     let elements = html.select(&selector);
     for element in elements {
         let url = element.value().attr("href")?;
-        println!("Url: {}", url);
+        if let Some(url) = normalize_url(domain_address, url) {
+            links.push(url);
+        }
+    }
+
+    Some(links)
+}
+
+pub fn normalize_url(domain_address: &str, url_source: &str) -> Option<String> {
+    let url = Url::parse(url_source);
+
+    if let Ok(url) = url {
+        if url.has_host() && url.host_str().unwrap() == domain_address {
+            return Some(url.to_string());
+        }
+    } else if url_source.starts_with('/') {
+        return Some(domain_address.to_owned() + url_source);
     }
 
     None
